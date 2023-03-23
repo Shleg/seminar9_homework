2d80b5843da54085754ae002b3487ca586f2bf0b
Andrew Shlegel <a.shlegel@outlook.com>
2023-03-23 20:32:25 -0300

    Task064 added

---
+++ Task064/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int InputUserData(string messageToUser)
+{
+    int result;
+    Console.Write(messageToUser);
+    string userAnswer = string.Empty;
+    while (!(int.TryParse(Console.ReadLine(), out result) & (result > 0)))
+    {
+        Console.WriteLine("You must enter an integer greater than 0");
+        Console.Write(messageToUser);
+    }
+    
+    return result;
+}
+
+void PrintNto1(int num)
+{
+    Console.Write(num + ", ");
+    if (num != 2) PrintNto1(num - 1);
+    else Console.WriteLine(num - 1);
+    
+}
+
+int number = InputUserData("Введите целое число больше 0: ");
+Console.Write($"N = {number} -> ");
+PrintNto1(number);
